c580188a9c9339917b8721aa34f114981a38ac43
Jonathan Chiang <jcdev@Jonathans-Mac-mini.local>
2018-03-20 09:51:03 -0500

    updated connection to server for heroku

--- src/helpers/connections.js
+++ src/helpers/connections.js
@@ -1,2 +1,3 @@
 // export const DEV_SERVER_URI = 'http://localhost:5000';
-export const DEV_SERVER_URI = 'http://10.10.10.4:5000';
+// export const DEV_SERVER_URI = 'http://10.10.10.4:5000';
+export const DEV_SERVER_URI = 'https://safe-brook-45797.herokuapp.com/';
